4dc83f51e89833ea025d641da4eaaaa0e5df1404
Darius <dariuskumza@gmail.com>
2023-11-10 21:47:30 +0200

    fetching data to trailers section

--- src/app.js
+++ src/app.js
@@ -1,24 +1,3 @@
-// async function displayTopRatedMovies() {
-//    const result = await fetchAPIData("/movie/top_rated");
-//    console.log(result);
-// }
-
-// // fetch data
-// async function fetchAPIData(endpoint) {
-//    const API_KEY = "1563fbe64685383509de99f4f1e8f7e5";
-//    const API_URL = "https://api.themoviedb.org/3";
-
-//    const response = await fetch(
-//       `${API_URL}/${endpoint}?api_key=${API_KEY}?language=en-US`
-//    );
-
-//    const data = await response.json();
-
-//    return data;
-// }
-
-// displayTopRatedMovies();
-
 const options = {
    method: "GET",
    headers: {
@@ -28,6 +7,7 @@ const options = {
    },
 };
 
+// * fetch top rated movies
 fetch(
    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
    options
@@ -43,13 +23,38 @@ const topMovies = [];
 
 const showTop5 = (topMovies) => {
    const showToDom = document.querySelector(".latest-cont");
+   //    console.log(topMovies);
    showToDom.innerHTML = topMovies
       .slice(0, 5)
       .map(
          (val) => `
-        <div class="m-c movies-2 flex justify-center items-center transition transform hover:scale-105">
-            <img class="object-cover h-96" src="https://image.tmdb.org/t/p/w300${val.poster_path}" alt="">
+        <div class="m-c transition transform hover:scale-105 duration-500 h-fit">
+            <img class="object-contain w-40 min-w-full" src="https://image.tmdb.org/t/p/original${val.poster_path}" alt="">
         </div>`
       )
       .join("");
 };
+
+const showLatest3Trailers = (arr, num) => {
+   const trailersWrap = document.querySelector(".trailers-wrap");
+
+   const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+   return shuffled.slice(0, num);
+   //    console.log(trailersWrap);
+};
+
+const nowPlayingMovies = [];
+// * fetch latest movies
+fetch(
+   "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+   options
+)
+   .then((response) => response.json())
+   .then((response) => {
+      nowPlayingMovies.push(...response.results);
+      console.log(nowPlayingMovies);
+      showLatest3Trailers(nowPlayingMovies);
+      // showTop5(topMovies);
+   })
+   .catch((err) => console.error(err));
